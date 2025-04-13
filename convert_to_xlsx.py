21d767621dafaf599576918b36797f4bc19db182
Lasitha Weligampola <lasiprabo@gmail.com>
2025-04-13 17:18:32 -0400

    csv file path update in the converter

--- convert_to_xlsx.py
+++ convert_to_xlsx.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-df = pd.read_csv("analyzed_results.csv", encoding="utf-8")
-df.to_excel("analyzed_results.xlsx", index=False, engine="openpyxl")
+df = pd.read_csv("./output/analyzed_results.csv", encoding="utf-8")
+df.to_excel("./output/analyzed_results.xlsx", index=False, engine="openpyxl")
